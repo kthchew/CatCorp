3fa3df0a939231af7b5b85ad6a6c54f3a5d7f26b
MaximilianMeiler <maximilianmeiler@ufl.edu>
2024-01-22 11:15:56 -0500

    Assignments route on backend
    NOTE
    courses aren't returning the exact correct id rn due to rounding issues

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 const app = express();
 app.use(cors());
 
-
+const config = {   transformResponse: [(data) => JSONbig.parse(data)], };
 
 app.get('/getCourses', async (req, res) => {
   const canvas_api_token = req.query.canvas_api_token;
@@ -17,18 +17,42 @@ app.get('/getCourses', async (req, res) => {
   try {
     const response = await axios.get(`https://canvas.instructure.com/api/v1/courses/`, {
       params: {
-        'access_token': canvas_api_token
-      }
+        'access_token': canvas_api_token,
+        "per_page": "100"
+      },
+      config
     });
-    const ids = response.data.map(course => course.id);
-    return res.json({ ids });
+    console.log(response)
+    // const ids = response.data.map(course => course);
+    return res.json(response.data);
   } catch (error) {
-    console.error('Error fetching files:', error);
-    res.status(200).json({ message: "No files available" });
+    console.error('Error fetching courses:', error);
+    res.status(200).json({ message: "No courses available" });
   }
 });
 
+app.get('/getAssignments', async (req, res) => {
+  const canvas_api_token = req.query.canvas_api_token;
+  const course_id = req.query.course_id;
 
+  if (!canvas_api_token || !course_id) {
+    return res.status(400).json({ error: 'canvas_api_token and course_id are required' });
+  }
+
+  try {
+    const response = await axios.get(`https://canvas.instructure.com/api/v1/courses/${course_id}/assignments`, {
+      params: {
+        'access_token': canvas_api_token,
+        "per_page": "100"
+      },
+    });
+    const ids = response.data.map(course => course);
+    return res.json({ ids });
+  } catch (error) {
+    console.error('Error fetching assignments:', error);
+    res.status(200).json({ message: "No assignments available" });
+  }
+});
 
 app.get('/', async (req, res) => {
   res.status(200).json({ message: "hello!" });
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,17 +3,18 @@ import axios from 'axios'
 import './App.css'
 
 const API_URL = "http://localhost:3500"
+const API_KEY = "1016~yq9pdLLpNzxvv8av456xSyWIzCA5MWHdbjODqaPCG6F3g2c351rknG6Zkf99RDwr"
 
 function App() {
-  const [courseIds, setCourseIds] = useState([])
+  const [courseIds, setCourseIds] = useState(null)
 
   const getCourses = async () => {
     const res = await axios.get(`${API_URL}/getCourses`, {
       params: {
-        "canvas_api_token": "1016~yq9pdLLpNzxvv8av456xSyWIzCA5MWHdbjODqaPCG6F3g2c351rknG6Zkf99RDwr"
-      }
+        "canvas_api_token": API_KEY
+      }   
     })
-    setCourseIds(res.data.ids);
+    setCourseIds(res.data);
   }
 
   useEffect(() => {
@@ -24,9 +25,9 @@ function App() {
   return (
     <>
       <div>Your course IDs:</div>
-      {courseIds.length != 0 ? 
-        courseIds.map((id) => {
-          return <div key={id}>{id}</div>
+      {courseIds ? 
+        courseIds.map((c) => {
+          return <div key={c.id}>{c.name} - {c.id}</div>
         })
       : 
         <div>Loading...</div>
