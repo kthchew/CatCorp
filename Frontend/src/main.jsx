5e15d40574c3b28290eac06ede59610b36d66c69
jasonlin15 <jasonlin1@ufl.edu>
2024-01-26 16:29:56 -0500

    replace

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import Login from './Login.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Login />
   </React.StrictMode>,
 )
