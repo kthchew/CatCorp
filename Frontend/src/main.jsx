c2badd0a299066ba6120a0046df66aa81cdef1b7
MaximilianMeiler <113135205+MaximilianMeiler@users.noreply.github.com>
2024-01-26 16:34:58 -0500

    Reimported index styles into main

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Login from './Login.jsx'
+import './index.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
