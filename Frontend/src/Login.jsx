895b4ed2beda2d0885c23a1ee5c554398b53d12f
jasonlin15 <jasonlin1@ufl.edu>
2024-02-12 21:43:16 -0500

    temp UI, working on leaderboard...

--- Frontend/src/Login.jsx
+++ Frontend/src/Login.jsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
 import axios from "axios"
-import logo from "./img/temp.png"
+import logo from "./img/title.png"
+import login_button from "./img/Login_Button.png"
+import create_account from "./img/Create_Account.png"
+import new_user from "./img/New_User.png"
+import exist_user from "./img/Existing_User.png"
 import PropTypes from "prop-types";
+import leaderWindow from "./img/Leader.png"
 import "./css/Login.css"
 
 const API_URL = "http://localhost:3500"
-
+var loginAcc = login_button
+var userState = new_user
 export default function Login({setLoginTime, apiKey, setApiKey, setUserData}) {
   const [username, setUser] = useState("");
   const [password, setPassword] = useState("");
@@ -59,13 +65,22 @@ export default function Login({setLoginTime, apiKey, setApiKey, setUserData}) {
   const toggleLoginState = () => {
     if (logState === "login") {
       setLogState("create")
+      loginAcc = create_account
+      userState = exist_user
     } else {
       setLogState("login")
+      loginAcc = login_button
+      userState = new_user
     }
   }
 
   return (
     <div className="loginContainer">
+      <div className="leader">
+        <button>
+          <img src={leaderWindow}/>
+        </button>
+      </div>
       <img src={logo} alt="Cat Corporate" className="loginLogo"/>
       <h2 className="loginHeader">Welcome to Cat Corporate!!!</h2>
       <form onSubmit={(e)=> handleSubmit(e)}>
@@ -85,12 +100,17 @@ export default function Login({setLoginTime, apiKey, setApiKey, setUserData}) {
           logState === "create" && <CanvasAPIKeyContainer keyVal={apiKey}
                                                           setKey={setApiKey}/>
         }
-        <button type="submit">{logState === "login" ? "Login" : "Create Account"}</button>
+        <div className="loginAccount">
+          <button type="submit">
+            <img src={loginAcc}/>
+          </button>
+        </div>
       </form>
       <div className="bottomForm">
         <div className="links">
-          <button onClick={toggleLoginState}>{logState === "login" ? "New User?" : "Existing User?"}</button>
-          <p>Forgot Password?</p>
+          <button onClick={toggleLoginState}>
+            <img src={userState}/>
+          </button>
         </div>
       </div>
       <small className="copyright">&copy; 2024 Cat Corporate</small>
