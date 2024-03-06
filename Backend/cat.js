7ebb62d682708e7cdba78ad6e26f23d6b19e8659
MonsieurMantis <S.Christensen1>
2024-03-06 15:58:35 -0500

    Cat Object Update

--- Backend/cat.js
+++ Backend/cat.js
@@ -1,8 +1,7 @@
 // Ordered by rarity. Least rare first.
-// TODO: actually put stuff here
-const CAT_EYES = ["test eyes 1", "test eyes 2"];
-const CAT_COLORS = ["test color 1", "test color 2"];
-const CAT_HATS = ["test hat 1", "test hat 2"];
+const CAT_EYES = ["OrangeEyes", "GreenEyes", "Blue Eyes", "GoldEyes"];
+const CAT_COLORS = ["OrangePix", "BrownPix", "whitePix", "TabbyPixel", "TShellPixel", "CalicoPix", "BlackPix"];
+const CAT_HATS = ["BluePinkParty", "Bowler", "CowboyHat", "PurpPinkParty", "BlueWiz", "Goggles", "PinkCowboayHat", "PurpWiz", "EarthParty", "Crown"];
 
 export default class Cat {
   // Generate a random cat.
@@ -16,5 +15,9 @@ export default class Cat {
     this.eye = CAT_EYES[eyeIndex];
     this.color = CAT_COLORS[colorIndex];
     this.hat = CAT_HATS[hatIndex];
+    this.rarity = eyeIndex + colorIndex + hatIndex;
+    this.name = "Placeholder until we can get the cat name randomizer working (string)";
+    this.imageXY = "Placeholder until we get the pixel coordinate randomizer working (pair)";
+    this.alive = true;
   }
 }
