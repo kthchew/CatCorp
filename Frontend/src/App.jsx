fd23ba845b4958df04f30a8de2d8dacb91e2d595
MaximilianMeiler <maximilianmeiler@ufl.edu>
2024-03-11 21:54:38 -0400

    Cat display from db

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -51,45 +51,51 @@ COURSE STORAGE - NEW MODEL
 
 
   return (
-    // <div>
-    //   {overlay == "login" ? 
-    //     <Login apiKey={apiKey} setApiKey={setApiKey} setUserData={setUserData} setUserId={setUserId} setCourses={setCourses}/>
-    //   :
-    //     <div>
-    //       <div>
-    //         <div className='floor'></div>
-    //         <div className='back'></div>
-    //         <div className='wall'></div>
-    //       </div>
+    <div>
+      {overlay == "login" ? 
+        <Login apiKey={apiKey} setApiKey={setApiKey} setUserData={setUserData} setUserId={setUserId} setCourses={setCourses}/>
+      :
+        <div>
+          <div>
+            <div className='floor'></div>
+            <div className='back'></div>
+            <div className='wall'></div>
+          </div>
 
-    //       <h3>Your cash: {userData.gems}</h3>
-    //       {/* <Rewards courses={courses} userData={userData}/> */}
-    //       <h1>Your course info {userId ? <>(UID: {userId})</> : <></>}</h1>
-    //       {courses && courses.message != "No courses available" ? 
-    //         courses.map((c) => {
-    //           return <div key={c[0]}>
-    //             <h3>{c[1]} - {c[0]}</h3>
-    //             {c[2].map((a) => {
-    //               return <div key={a}>
-    //                 <h4 style={{color:'lightgreen'}}>{a[1]} - {a[0]}</h4>
-    //               </div>
-    //             })}
-    //             {c[3].map((a) => {
-    //               return <div key={a}>
-    //                 <h4 style={{color:'orange'}}>{a[1]} - {a[0]}</h4>
-    //               </div>
-    //             })}
-    //           </div>
-              
-        
-    //         })
-    //       : 
-    //         <h2>Finishing up...</h2>
-    //       }
-    //     </div>
-    //   }
-    //   </div>
-    <Cat eyes={"blue"} hat={"crown"} pattern={"calico"}/>
+          <div>
+            {userData.cats.map((cat, i) => {
+              return <Cat eyes={cat.eyes} hat={cat.hat} pattern={cat.pattern} patX={cat.x} patY={cat.y} x={10} y={10 + 10*i} key={i}/>  
+            })}
+          </div>
+
+          {/* <h3>Your cash: {userData.gems}</h3> */}
+          {/* <Rewards courses={courses} userData={userData}/> */}
+          {/* <h1>Your course info {userId ? <>(UID: {userId})</> : <></>}</h1> */}
+          {/* {courses && courses.message != "No courses available" ? 
+            courses.map((c) => {
+              return <div key={c[0]}>
+                <h3>{c[1]} - {c[0]}</h3>
+                {c[2].map((a) => {
+                  return <div key={a}>
+                    <h4 style={{color:'lightgreen'}}>{a[1]} - {a[0]}</h4>
+                  </div>
+                })}
+                {c[3].map((a) => {
+                  return <div key={a}>
+                    <h4 style={{color:'orange'}}>{a[1]} - {a[0]}</h4>
+                  </div>
+                })}
+              </div>
+            })
+          : 
+            <h2>Finishing up...</h2>
+          } */}
+
+      
+        </div>
+      }
+      </div>
+
   )
 }
 
--- Frontend/src/Cat.jsx
+++ Frontend/src/Cat.jsx
@@ -3,13 +3,19 @@ import PropTypes from "prop-types";
 
 import catTemplate from './img/CatTemplate.png'
 
-function Cat({eyes, hat, pattern}) {
+function Cat({eyes, hat, pattern, patX, patY, x, y}) {
   return (
-    <div className='cat'>
+    <div className='cat' style={{bottom: y, left: x}}>
       <img className='catTemplate' src={catTemplate}></img>
       <img className='catTemplate' src={`eyes/${eyes}.png`}></img>
       <img className='catTemplate' src={`hats/${hat}.png`}></img>
-      <img className='catPelt' src={`pelts/${pattern}.jpg`} style={{}}></img>
+      <img className='catPelt' 
+        src={`pelts/${pattern}.jpg`} 
+        style={{
+          transform: `translate(calc((-100% + 64px) * ${patX}), calc((-100% + 64px) * ${patY}))`,
+          maskPosition: `${patX * 100}% ${patY * 100}%`
+        }}
+      ></img>
     </div>
   )
 }
@@ -20,4 +26,8 @@ Cat.propTypes = {
   eyes: PropTypes.string,
   hat: PropTypes.string,
   pattern: PropTypes.string,
+  patX: PropTypes.number,
+  patY: PropTypes.number,
+  x: PropTypes.number,
+  y: PropTypes.number,
 }
