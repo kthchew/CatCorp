7f0f3f6ddb791d4e79d6a475c61030f1a908d4a6
Kenneth Chew <79120643+kthchew@users.noreply.github.com>
2024-03-26 20:55:55 -0400

    Store CSS overhaul

--- Frontend/src/Store.jsx
+++ Frontend/src/Store.jsx
@@ -1,47 +1,49 @@
+import axios from "axios";
 import "./css/Store.css"
 import PropTypes from "prop-types";
 import loot_box from "./img/UI/lootbox.png"
-import store_UI from "./img/UI/store_window.png"
 import close_button from "./img/UI/close_button.png"
 import adopt_button from "./img/UI/Adopt.png"
-import display_frame from "./img/UI/display_frame.png"
 import gem_count from "./img/UI/gem_display.png"
-import axios from "axios"
-
-
 
 export default function Store ({setOverlay, userData, setUserData}) {
-
-  async function buyLootboxTest() {
+  async function buyLootbox(id) {
+    // TODO: modify based on reply `cats` and `spent`
     try {
-      const lootboxResp = await axios.post(`/buyLootbox`, {lootboxID: "1"});
+      const lootboxResp = await axios.post(`/buyLootbox`, {lootboxID: id});
       setUserData({...userData, gems: userData.gems - lootboxResp.data.spent, cats: [...userData.cats, lootboxResp.data.cat]});
     } catch (e) {
+      // TODO: tell user it failed
       console.error("Purchase failed");
     }
   }
 
   return (
-      <div className="store_container">
-        <button className="close_store" onClick={() => setOverlay("home")}>
-          <img src={close_button}/>
-        </button>
-        <div className="gem_display" >
-          <img src={gem_count} style={{marginRight: "-55px"}}/>
-
-          <div style={{zIndex: 10, width:"55px", fontWeight:"bold"}}>{userData.gems}</div>
-        </div>
-        <img src={store_UI}/>
-        <div className="display">
-          <img src={display_frame} id="display_frame"/>
-          <div className="lootbox">
-            <img src={loot_box}/>
+      <div className="store_outer_container">
+        <div className="store_ears"></div>
+        <div className="store_container">
+          <div className="store_header">
+            <button className="close_store" onClick={() => setOverlay("home")}>
+              <img src={close_button}/>
+            </button>
+            <div className="gem_display">
+              <img src={gem_count}/>
+              <p>{userData.gems}</p>
+            </div>
           </div>
-        </div>  
-        <div className="adopt" onClick={() => buyLootboxTest()}>
-          <button className="adopt_button" >
-            <img src={adopt_button}/>
-          </button>
+          <div className="lootbox_collection">
+            {
+              <div className="lootbox_container">
+                <div className="display">
+                  <img className="lootbox" src={loot_box}/>
+                  <p>Cost: ???</p>
+                </div>
+                <button className="adopt_button" onClick={() => buyLootbox("1")}>
+                  <img src={adopt_button}/>
+                </button>
+              </div>
+            }
+          </div>
         </div>
       </div>
   );
