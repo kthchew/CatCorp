cd831219d4ba7fc6067ddd67031c3d0472740995
MaximilianMeiler <maximilianmeiler@ufl.edu>
2024-04-08 16:18:19 -0400

    Appease linter

--- Frontend/src/Checklist.jsx
+++ Frontend/src/Checklist.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import "./css/Rewards.css"
-import gem from "../src/img/gem.png"
 
 export default function Rewards({courses, setOverlay}) {
   const [subArr, setSubArr] = useState([])
