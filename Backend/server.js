7ba664496ffab74322aa5252ed46f78a11253223
Kenneth Chew <79120643+kthchew@users.noreply.github.com>
2024-04-23 22:50:54 -0400

    Fix partitioned set incorrectly

--- Backend/server.js
+++ Backend/server.js
@@ -34,7 +34,7 @@ app.use(session({
   secret: SESSION_SECRET,
   maxAge: 24 * 60 * 60 * 1000, // 1 day
   sameSite: usingLocalhost ? 'lax' : 'none',
-  partitioned: true,
+  partitioned: usingLocalhost ? false : true,
   secure: usingLocalhost ? false : true,
 }))
 app.use(lusca({
